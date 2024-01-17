f40117ea7f5e52b6d49e909ccddb3edea6a09a93
alasaidi <alasaidi55@gmail.com>
2024-01-17 22:09:49 +0100

    adding the button to show more

--- src/script.js
+++ src/script.js
@@ -7,7 +7,7 @@ let keyword = "";
 let page = 1;
 async function searchImages() {
   keyword = searchBox.value;
-  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
   const response = await fetch(url);
   const data = await response.json();
   //   console.log(data);
@@ -21,9 +21,14 @@ async function searchImages() {
     imageLink.appendChild(image);
     searchResult.appendChild(imageLink);
   });
+  showMoreBtn.style.display = "block";
 }
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
   page = 1;
+  searchImages();
+});
+showMoreBtn.addEventListener("click", () => {
+  page++;
   searchImages();
 });
