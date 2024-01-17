ffdd4a38e6c2d664620cece61ff1bcc6683504f6
alasaidi <alasaidi55@gmail.com>
2024-01-17 22:01:55 +0100

    display the images in the page

--- src/script.js
+++ src/script.js
@@ -10,7 +10,17 @@ async function searchImages() {
   const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
   const response = await fetch(url);
   const data = await response.json();
-  console.log(data);
+  //   console.log(data);
+  const results = data.results;
+  results.map((result) => {
+    const image = document.createElement("img");
+    image.src = result.urls.small;
+    const imageLink = document.createElement("a");
+    imageLink.href = result.links.html;
+    imageLink.target = "_blank";
+    imageLink.appendChild(image);
+    searchResult.appendChild(imageLink);
+  });
 }
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
